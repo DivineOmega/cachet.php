103693d82b11c08d0901c513e579814792a049a5
Jordan Hall <jordan@hall05.co.uk>
2015-05-20 20:43:16 +0100

    Retrieval of components, incidents and metrics

---
+++ cachet.php
@@ -0,0 +1,89 @@
+<?php
+namespace DivineOmega\CachetPHP;
+
+class CachetPHP
+{
+    private $baseURL = '';
+    private $email = '';
+    private $password = '';
+    
+    public function __construct()
+    {
+        
+    }
+    
+    public function setBaseURL($baseURL)
+    {
+        $this->baseURL = $baseURL;
+    }
+    
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+    
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+    
+    private function sanityCheck($authorisationRequired)
+    {
+        if (!$this->baseURL)
+        {
+            throw new Exception('cachet.php: The base URL is not set for your cachet instance. Set one with the setBaseURL method.');
+        }
+        else if ($authorisationRequired && !$this->email)
+        {
+            throw new Exception('cachet.php: The email is not set for your cachet instance. Set one with the setEmail method.');
+        }
+        else if ($authorisationRequired && !$this->password)
+        {
+            throw new Exception('cachet.php: The password is not set for your cachet instance. Set one with the setPassword method.');
+        }
+    }
+    
+    private function get($type)
+    {
+        if ($type!=='components' && $type!=='incidents' && $type!=='metrics')
+        {
+            throw new Exception('cachet.php: Invalid type specfied. Must be \'components\', \'incidents\' or \'metrics\'.');
+        }
+        
+        $this->sanityCheck(false);
+        
+        $url = $this->baseURL . $type;
+        
+        $ch = curl_init($url);
+        
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        
+        $response = curl_exec($ch);
+        curl_close($ch);
+        
+        $data = json_decode($response);
+        
+        if (isset($data->data) && $data->data)
+        {
+            $data = $data->data;
+        }
+        
+        return $data;
+    }
+    
+    public function getComponents()
+    {
+        return $this->get('components');
+    }
+    
+    public function getIncidents()
+    {
+        return $this->get('incidents');
+    }
+    
+    public function getMetrics()
+    {
+        return $this->get('metrics');
+    }
+}
---
+++ example.php
@@ -0,0 +1,52 @@
+<?php
+require_once 'cachet.php';
+
+use DivineOmega\CachetPHP\CachetPHP;
+
+// Create a CachetPHP object
+$cachetPHP = new CachetPHP();
+
+// Set the base URL for your Cachet instance API
+$cachetPHP->setBaseURL('https://demo.cachethq.io/api/');
+
+// If you need to change things, you need to provide the email and password you user to log in to the Cachet dashboard
+$cachetPHP->setEmail('test@test.com');
+$cachetPHP->setPassword('test123');
+
+// Get components
+$components = $cachetPHP->getComponents();
+
+// Display components
+echo '*** Components ***';
+echo "\n";
+foreach($components as $component)
+{
+    echo $component->id.' - '.$component->name.' - '.$component->description.' - '.$component->status;
+    echo "\n";
+}
+
+// Get incidents
+$incidents = $cachetPHP->getIncidents();
+
+// Display incidents
+echo '*** Incidents ***';
+echo "\n";
+foreach($incidents as $incident)
+{
+    echo $incident->id.' - '.$incident->name.' - '.$incident->message.' - '.$incident->human_status;
+    echo "\n";
+}
+
+// Get metrics
+$metrics = $cachetPHP->getMetrics();
+
+// Display components
+echo '*** Metrics ***';
+echo "\n";
+foreach($metrics as $metric)
+{
+    echo $metric->id.' - '.$metric->name;
+    echo "\n";
+}
+
+?>
